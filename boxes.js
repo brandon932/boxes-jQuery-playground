044fae8c7baabcf44606aeb579a97e3bf11b4717
brandon932 <bthornton_932@yahoo.com>
2015-07-31 13:07:31 -0600

    added dogToggle function

--- boxes.js
+++ boxes.js
@@ -1,1 +1,40 @@
-console.log("hello world");
+$(document).ready(function(){
+  // $("#secretBox").css("back ground-color", "white");
+  // $("#secretBox").html('<h1>Secret Box!</h1>');
+  // $("#secretBox").css("color","black");
+  // $("#row1").children().attr("class","box");
+  // $("#row1").children().addClass("boxType3");
+  // $(".boxType1").css("background-color","white");
+  // $("#row2>:lt(2)").css("visibility","hidden");
+  // $("div:not([id])").css("width","2px");
+  // $("#container").on("click",function(event){
+  //     console.log($(event.pageX), $(event.pageY) );
+  // });
+  // $(".boxType1").wrap("<a href='https://students.galvanize.com/'></a>").on("click", function(event){
+    // if(confirm("You can Never leave this page"))
+      // event.preventDefault();
+  // });
+  function dogToggle(){
+    $(".box").on("click",function(){
+      $(this).html("<img class='dog' src='tiny.jpg'>");
+      console.log("test");
+    });
+    $(".box").on("click",".dog",function(event){
+      console.log("hello");
+      event.stopPropagation();
+      $(this).toggle();
+    });
+  }
+  // dogToggle();
+  function blackOnWhite(){
+    $("#container").on("click",function(){
+    $(this).css("background-color","black");
+  });
+
+    $(this).children().child(3).css("background-color","white");
+
+
+  }
+  blackOnWhite();
+
+});
